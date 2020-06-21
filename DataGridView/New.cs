6e0c4c4b9a86ca80f5dd3516172c7164d2f40469
Piramidka <vetersnami64@yandex.ru>
2020-06-21 15:59:24 +0300

    Добавьте файлы проекта.

---
+++ DataGridView/New.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DataGridView
+{
+    public partial class New : Form
+    {
+        public New()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
